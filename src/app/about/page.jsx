ccdc67364b7808c97c79a8bad489547c17e3a781
lucas <lucas.chorolqui@gmail.com>
2024-03-28 21:15:15 -0300

    updates

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,35 @@
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "../componentes/components/ui/command";
+
 function AboutPage() {
   return (
     <section className="container mx-auto">
+      <div>
+        <Command>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </div>
       <h3 className="text-4xl font-bold my-5">About</h3>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum harum
